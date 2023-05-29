a66a57c95406e867955539f1c37f333ce5befe74
steven.yeo <steven.y@dynrtech.com>
2023-05-29 21:01:30 +0700

    latest changes for class component

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class ErrorBoundry extends Component {
+
+    constructor() {
+        super();
+        
+        this.state = {
+            hasError: false
+        };
+    }
+
+    componentDidCatch() {
+        this.setState({
+            hasError: true
+        });
+    }
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Oops got error!</h1>;
+        }
+
+        return this.props.children;
+    }
+}
+
+export default ErrorBoundry;
---
+++ src/containers/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+
+import CardList from '../components/CardList';
+
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
+
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            searchField: '',
+            robots: []
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({ robots: users }));
+    }
+
+    onSearchChange = (event) => {
+        this.setState({
+            searchField: event.target.value
+        });
+    }
+
+    render() {
+        const { robots, searchField } = this.state;
+        const filteredRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(searchField.toLowerCase());
+        });
+
+        return !robots.length ? 
+            <h1>Loading</h1> :
+            (
+                <div className='tc'>
+                    <h1 className='f2'>RoboFriends</h1>
+                    <SearchBox searchField={searchField} searchChange={this.onSearchChange}/>
+                    <Scroll>
+                        <ErrorBoundry>
+                            <CardList robots={filteredRobots} />
+                        </ErrorBoundry>
+                    </Scroll>
+                </div>
+            );
+    }    
+};
+
+export default App;
